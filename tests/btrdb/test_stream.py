04100d3cdfd95d0a6060e3eae7ef598b5986994e
looselycoupled <allen.leis@gmail.com>
2019-01-09 18:20:26 -0500

    adds potential fixes for freeze_time; removes dev pythons from Travis config

--- tests/btrdb/test_stream.py
+++ tests/btrdb/test_stream.py
@@ -16,6 +16,8 @@ Testing package for the btrdb stream module
 ##########################################################################
 
 import uuid
+import pytz
+import datetime
 import pytest
 from unittest.mock import Mock, PropertyMock
 from freezegun import freeze_time
@@ -405,7 +407,7 @@ class TestStream(object):
         stream = Stream(btrdb=BTrDB(endpoint), uuid=uu)
         endpoint.nearest = Mock(return_value=(RawPointProto(time=100, value=1.0), 42))
 
-        with freeze_time("2018-01-01 12:00:00 -0000"):
+        with freeze_time("2018-01-01 12:00:00", tz_offset=0):
             point, ver = stream.latest()
 
         assert (point, ver) == (RawPoint(100, 1.0), 42)
@@ -421,7 +423,7 @@ class TestStream(object):
         stream = Stream(btrdb=BTrDB(endpoint), uuid=uu)
         endpoint.nearest = Mock(side_effect=Exception())
 
-        with freeze_time("2018-01-01 12:00:00 -0000"):
+        with freeze_time(datetime.datetime(2018,1,1,12, tzinfo=pytz.utc)):
             assert stream.latest() is None
         endpoint.nearest.assert_called_once_with(uu, 1514826000000000000, 0, True)
 
