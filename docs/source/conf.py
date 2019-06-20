cf651961a680f0b42ea87d2cee379a6741f0e804
looselycoupled <allen.leis@gmail.com>
2019-06-20 16:39:49 -0400

    explicitly adds intersphinx extension to docs [ch1627]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,6 +49,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
     'sphinx.ext.githubpages',
+    'sphinx.ext.intersphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
