e1a7ef51f1ea82761f8a29019dbe5c192cab0756
looselycoupled <allen.leis@gmail.com>
2021-10-29 11:25:02 -0400

    Release v5.12.0

--- btrdb/version.py
+++ btrdb/version.py
@@ -15,7 +15,7 @@ Maintains version and package information for deployment.
 ## Module Info
 ##########################################################################
 
-__version_info__ = { 'major': 5, 'minor': 11, 'micro': 7, 'releaselevel': 'final'}
+__version_info__ = { 'major': 5, 'minor': 12, 'micro': 0, 'releaselevel': 'final'}
 
 ##########################################################################
 ## Helper Functions
